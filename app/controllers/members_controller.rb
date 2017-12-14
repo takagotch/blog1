0e249d507c97f0d592815258a226bf430341916c
takagotch <dyaccb@gmail.com>
2017-12-14 19:28:30 +0900

    members#new|edit && _form

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,5 +12,13 @@ def show
 	@member = Member.find(params[:id])
 end
 
+def new
+	@member = Member.new(birthday: Date.new(1994, 1, 1))
+end
+
+def edit
+	@member = Member.find(params[:id])
+end
+
 end
 

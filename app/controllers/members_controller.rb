a54843fd908a8c547ab5995029ccb3110805412b
takagotch <dyaccb@gmail.com>
2017-12-14 20:20:40 +0900

    csrf_meta_tags

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -20,5 +20,29 @@ def edit
 	@member = Member.find(params[:id])
 end
 
+def create
+	@member = Member.new(params[:member])
+	if @member.save
+		redirect_to @member, notice: "member_register"
+	else
+		render "new"
+	end
+end
+
+def update
+	@member = Member.find(params[:id])
+	@member.assign_attributes(params[:member])
+	if @member.save
+	  redirect_to @member, notice: "update_member_info"
+	else
+	end
+end
+
+def destroy
+	@member = Member.find(params[:id])
+	@member.destroy
+	redirect_to :members, notice: "delete_member"
+end
+
 end
 

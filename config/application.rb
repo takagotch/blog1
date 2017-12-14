c0b22a00f71a71548a026f474913a6fe90b0e146
takagotch <dyaccb@gmail.com>
2017-12-14 20:10:04 +0900

    dev_only: strongparameter

--- config/application.rb
+++ config/application.rb
@@ -4,3 +4,9 @@ module takagotch
 
   config.time_zone = 'Osaka'
 
+  config.active_record.raise_in_transactional_callbacks = true
+  
+  #dev
+  #config.action_controller.permit_all_parameters = true
+  end
+end
